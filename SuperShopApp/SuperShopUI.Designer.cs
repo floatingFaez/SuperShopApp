3bf8fd59d083eeee16231e0d16784963f355e72d
floatingFaez <floatingfaez@live.com>
2014-10-16 23:38:32 +0600

    Primary All File Uploaded. Here I Use Dictionary or List

---
+++ SuperShopApp/SuperShopUI.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SuperShopApp
+{
+    partial class SuperShopUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.shopSaveButton = new System.Windows.Forms.Button();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.productSaveButton = new System.Windows.Forms.Button();
+            this.quentityTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.productIdTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.showDetailsButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.shopSaveButton);
+            this.groupBox1.Controls.Add(this.addressTextBox);
+            this.groupBox1.Controls.Add(this.nameTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 124);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Shop Info";
+            // 
+            // shopSaveButton
+            // 
+            this.shopSaveButton.Location = new System.Drawing.Point(278, 84);
+            this.shopSaveButton.Name = "shopSaveButton";
+            this.shopSaveButton.Size = new System.Drawing.Size(68, 30);
+            this.shopSaveButton.TabIndex = 2;
+            this.shopSaveButton.Text = "Save";
+            this.shopSaveButton.UseVisualStyleBackColor = true;
+            this.shopSaveButton.Click += new System.EventHandler(this.shopSaveButton_Click);
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(105, 58);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(241, 20);
+            this.addressTextBox.TabIndex = 1;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(105, 34);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(241, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.productSaveButton);
+            this.groupBox2.Controls.Add(this.quentityTextBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.productIdTextBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 142);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(381, 127);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Product Info";
+            // 
+            // productSaveButton
+            // 
+            this.productSaveButton.Location = new System.Drawing.Point(278, 76);
+            this.productSaveButton.Name = "productSaveButton";
+            this.productSaveButton.Size = new System.Drawing.Size(68, 30);
+            this.productSaveButton.TabIndex = 2;
+            this.productSaveButton.Text = "Save";
+            this.productSaveButton.UseVisualStyleBackColor = true;
+            this.productSaveButton.Click += new System.EventHandler(this.productSaveButton_Click);
+            // 
+            // quentityTextBox
+            // 
+            this.quentityTextBox.Location = new System.Drawing.Point(105, 50);
+            this.quentityTextBox.Name = "quentityTextBox";
+            this.quentityTextBox.Size = new System.Drawing.Size(241, 20);
+            this.quentityTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Product/Item Id";
+            // 
+            // productIdTextBox
+            // 
+            this.productIdTextBox.Location = new System.Drawing.Point(105, 26);
+            this.productIdTextBox.Name = "productIdTextBox";
+            this.productIdTextBox.Size = new System.Drawing.Size(241, 20);
+            this.productIdTextBox.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Quentity";
+            // 
+            // showDetailsButton
+            // 
+            this.showDetailsButton.Location = new System.Drawing.Point(117, 278);
+            this.showDetailsButton.Name = "showDetailsButton";
+            this.showDetailsButton.Size = new System.Drawing.Size(183, 30);
+            this.showDetailsButton.TabIndex = 2;
+            this.showDetailsButton.Text = "Show Details";
+            this.showDetailsButton.UseVisualStyleBackColor = true;
+            this.showDetailsButton.Click += new System.EventHandler(this.showDetailsButton_Click);
+            // 
+            // SuperShopUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 320);
+            this.Controls.Add(this.showDetailsButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SuperShopUI";
+            this.Text = "SuperShopUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button shopSaveButton;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button productSaveButton;
+        private System.Windows.Forms.TextBox quentityTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox productIdTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button showDetailsButton;
+    }
+}
+
---
+++ SuperShopApp/SuperShopUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperShopApp
+{
+    public partial class SuperShopUI : Form
+    {
+        private Shop aShop;
+        private Product aProduct;
+
+        public SuperShopUI()
+        {
+            InitializeComponent();
+        }
+
+        private void shopSaveButton_Click(object sender, EventArgs e)
+        {
+            aShop = new Shop(nameTextBox.Text,addressTextBox.Text);
+            MessageBox.Show(@"Shop is created");
+        }
+
+        private void productSaveButton_Click(object sender, EventArgs e)
+        {
+            int productId = Convert.ToInt32(productIdTextBox.Text);
+            int quentity = Convert.ToInt32(quentityTextBox.Text);
+            aProduct = new Product( productId, quentity);
+            string msg = aShop.Add(aProduct);
+            MessageBox.Show(msg);
+        }
+
+        private void showDetailsButton_Click(object sender, EventArgs e)
+        {
+            string msg = aShop.GetAllProduct(aProduct);
+            MessageBox.Show(msg);
+        }
+    }
+}
